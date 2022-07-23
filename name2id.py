4faad087a6c52282714823d30d3b686897c1280e
TettaMasukawa <ea.masukawa17@gmail.com>
2022-07-23 15:38:52 +0900

    renew

--- name2id.py
+++ name2id.py
@@ -1,53 +1,34 @@
-import requests
+import pandas as pd
+
+from twitter_api_connect import connect_to_endpoint, waitUntilReset
 
 
-def create_url(usernames, user_fields):
-    if any(usernames):
-        formatted_user_names = "usernames=" + ",".join(usernames)
-    else:
-        formatted_user_names = ""
+def get_id(BT, username):
+    url = "https://api.twitter.com/2/users/by"
+    params1 = {"usernames": "{}".format(username), "user.fields": "id"}
+    headers = {"Authorization": "Bearer {}".format(BT)}
+    response, json_response = connect_to_endpoint(url, params1, headers)
 
-    if any(user_fields):
-        formatted_user_fields = "user.fields=" + ",".join(user_fields)
-    else:
-        formatted_user_fields = "user_fields=id,name,username"
+    if int(response.headers["x-rate-limit-remaining"]) == 0:
+        waitUntilReset(response.headers["x-rate-limit-reset"])
+        
+    try:
+        id = json_response["data"][0]["id"]
+    except:
+        return False
 
-    url = "https://api.twitter.com/2/users/by?{}&{}".format(
-        formatted_user_names, formatted_user_fields
-    )
-    return url
-
-
-def create_headers(bearer_token):
-    headers = {"Authorization": "Bearer {}".format(bearer_token)}
-    return headers
-
-
-def connect_to_endpoint(url, headers):
-    response = requests.request("GET", url, headers=headers)
-    if response.status_code != 200:
-        raise Exception(
-            "Request returned an error: {} {}".format(
-                response.status_code, response.text
-            )
-        )
-    return response.json()
-
-
-def main():
-    BT = "AAAAAAAAAAAAAAAAAAAAAC%2FlQgEAAAAAi9yH0D1YnXy%2F4tKDFNd4Jw%2B9GyQ%3DukHqkVzZWJQX48nwMVys5VglneLIRW3CG566Z0ZY8SUCBkGPsa"
-    usernames = [
-        "poke_mopu",  # screenname here!
-    ]
-    user_fields = ["id"]
-
-    url = create_url(usernames, user_fields)
-    headers = create_headers(BT)
-    json_response = connect_to_endpoint(url, headers)
-    data = json_response["data"]
-
-    print(data)
-
+    return id
 
 if __name__ == "__main__":
-    main()
+    BT = "AAAAAAAAAAAAAAAAAAAAAC%2FlQgEAAAAAi9yH0D1YnXy%2F4tKDFNd4Jw%2B9GyQ%3DukHqkVzZWJQX48nwMVys5VglneLIRW3CG566Z0ZY8SUCBkGPsa"
+    csv = pd.read_csv("/disk/ssd14tc/tmasukawa/tweet-analysis/data/crowdsourcing/account_list.csv", encoding="utf-8-sig")
+    
+    id_list = []
+    
+    for n in range(len(csv)):
+        id = get_id(BT, csv["account"][n])
+        id_list.append(id)
+        
+    csv["id"] = id_list
+    
+    csv.to_csv("/disk/ssd14tc/tmasukawa/tweet-analysis/data/crowdsourcing/account_list.csv", encoding="utf-8-sig", mode="w")
